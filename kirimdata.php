a87213c74f609de6cb8e6074950d33c58006457d
rynarf25 <aripsatrio45@gmail.com>
2024-07-30 17:34:01 +0700

    updated

--- kirimdata.php
+++ kirimdata.php
@@ -5,9 +5,10 @@
     $suhu = $_GET["Suhu_udara"];  
     $kelembapan_udara = $_GET["Kelembapan_udara"];  
     $ph = $_GET["Kelembapan_tanah"];
+    $phtanah = $_GET["phtanah"];
     $tinggi = $_GET["Tinggi"];
     $status = $_GET["status"];
 
     mysqli_query($konek,"ALTER TABLE sensor AUTO_INCREMENT=1");
-    mysqli_query($konek, "INSERT INTO sensor(Suhu, kelembapan_udara, Ph_tanah, Tinggi, status)VALUES('$suhu', '$kelembapan_udara', '$ph', '$tinggi', '$status')");
+    mysqli_query($konek, "INSERT INTO sensor(Suhu, kelembapan_udara, Ph_tanah, phtanah2, Tinggi, status)VALUES('$suhu', '$kelembapan_udara', '$ph', '$phtanah', '$tinggi', '$status')");
 ?>
